6c15966891830c56378c21ca797024f6ad3c6c2f
ashwinn <ashwinn@qburst.com>
2022-12-03 20:18:46 -0800

    Initial

--- main.cu
+++ main.cu
@@ -58,6 +58,7 @@ int main (int argc, char *argv[])
     A_sz = matArow*matAcol;
     B_sz = matBrow*matBcol;
     C_sz = matArow*matBcol;
+
     VecSize = matArow*matAcol;
     const int segmentLen = VecSize / numStream;
 
@@ -207,20 +208,20 @@ int main (int argc, char *argv[])
 
     /*************************************************************************/
     //INSERT CODE HERE
-    // cudaMemcpy(C_h, C_d, sizeof(float) * C_sz, cudaMemcpyDeviceToHost);	
+    cudaMemcpy(C_h, C_d, sizeof(float) * C_sz, cudaMemcpyDeviceToHost);	
 
 
-    for (int i = 0; i < numStream; i++)
-    {
-        if (i != numStream-1)
-        {
-            cudaMemcpyAsync(C_h + i*segmentLen, C_d[i], sizeof(float)*segmentLen, cudaMemcpyDeviceToHost, streams[i]);
-        }
-        else
-        {
-            cudaMemcpyAsync(C_h + i*segmentLen, C_d[i], sizeof(float)*(segmentLen + VecSize % numStream), cudaMemcpyDeviceToHost, streams[i]);
-        }
-    }
+    // for (int i = 0; i < numStream; i++)
+    // {
+    //     if (i != numStream-1)
+    //     {
+    //         cudaMemcpyAsync(C_h + i*segmentLen, C_d[i], sizeof(float)*segmentLen, cudaMemcpyDeviceToHost, streams[i]);
+    //     }
+    //     else
+    //     {
+    //         cudaMemcpyAsync(C_h + i*segmentLen, C_d[i], sizeof(float)*(segmentLen + VecSize % numStream), cudaMemcpyDeviceToHost, streams[i]);
+    //     }
+    // }
 
 
 

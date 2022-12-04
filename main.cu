47557fe7dee915e6f2fb95d8ae427319aa4f95f3
ashwinn <ashwinn@qburst.com>
2022-12-03 23:48:51 -0800

    Initial

--- main.cu
+++ main.cu
@@ -188,7 +188,7 @@ int main (int argc, char *argv[])
         }
         else
         {
-            basicSgemmStream(VecSize, matArow/numStream + VecSize % numStream,matArow/numStream + VecSize % numStream,A_ds[i], B_ds[i], C_ds[i], streams[i]);
+            basicSgemmStream(2, matArow/numStream + VecSize % numStream,matArow/numStream + VecSize % numStream,A_ds[i], B_ds[i], C_ds[i], streams[i]);
         }
         cudaStreamSynchronize(streams[i]);
     }

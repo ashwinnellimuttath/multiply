df29e629610b94426378ff024649361714341ef0
ashwinn <ashwinn@qburst.com>
2022-12-03 21:14:41 -0800

    Initial

--- main.cu
+++ main.cu
@@ -129,8 +129,8 @@ int main (int argc, char *argv[])
 	
     /*************************************************************************/
     //INSERT CODE HERE
-    // cudaMemcpy(A_d, A_h, sizeof(float) * A_sz, cudaMemcpyHostToDevice);
-    // cudaMemcpy(B_d, B_h, sizeof(float) * B_sz, cudaMemcpyHostToDevice);
+    cudaMemcpy(A_d, A_h, sizeof(float) * A_sz, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B_h, sizeof(float) * B_sz, cudaMemcpyHostToDevice);
 
     // for (int i = 0; i < numStream; i++)
     // {
@@ -146,23 +146,23 @@ int main (int argc, char *argv[])
     //     }
     // }
 
-    for (int i = 0; i < numStream; i++)
-    {
-        int Offset = i * segmentLen;
-        cudaMemcpyAsync(&A_d[Offset], &A_h[Offset], sizeof(float)*segmentLen, cudaMemcpyHostToDevice, streams[i]);
-        cudaMemcpyAsync(&B_d[Offset], &B_h[Offset], sizeof(float)*segmentLen, cudaMemcpyHostToDevice, streams[i]);
+    // for (int i = 0; i < numStream; i++)
+    // {
+    //     int Offset = i * segmentLen;
+    //     cudaMemcpyAsync(&A_d[Offset], &A_h[Offset], sizeof(float)*segmentLen, cudaMemcpyHostToDevice, streams[i]);
+    //     cudaMemcpyAsync(&B_d[Offset], &B_h[Offset], sizeof(float)*segmentLen, cudaMemcpyHostToDevice, streams[i]);
         
-        basicSgemm(matArow/numStream, matArow/numStream, matArow/numStream, A_d[Offset], B_d[Offset], C_d[Offset], streams[i]);
-        // else
-        // {
-        //     cudaMemcpyAsync(A_d[i], A_h + i*segmentLen, sizeof(float)*(segmentLen + VecSize % numStream), cudaMemcpyHostToDevice, streams[i]);
-        //     cudaMemcpyAsync(B_d[i], B_h + i*segmentLen, sizeof(float)*(segmentLen + VecSize % numStream), cudaMemcpyHostToDevice, streams[i]);
-        // }
-        cudaMemcpyAsync(&C_h[Offset], &C_d[Offset], sizeof(float)*segmentLen, cudaMemcpyDeviceToHost, streams[i]);
-    }
+    //     basicSgemm(matArow/numStream, matArow/numStream, matArow/numStream, A_d[Offset], B_d[Offset], C_d[Offset], streams[i]);
+    //     // else
+    //     // {
+    //     //     cudaMemcpyAsync(A_d[i], A_h + i*segmentLen, sizeof(float)*(segmentLen + VecSize % numStream), cudaMemcpyHostToDevice, streams[i]);
+    //     //     cudaMemcpyAsync(B_d[i], B_h + i*segmentLen, sizeof(float)*(segmentLen + VecSize % numStream), cudaMemcpyHostToDevice, streams[i]);
+    //     // }
+    //     cudaMemcpyAsync(&C_h[Offset], &C_d[Offset], sizeof(float)*segmentLen, cudaMemcpyDeviceToHost, streams[i]);
+    // }
 
-    for (int i = 0; i < numStream; i++)
-        cudaStreamSynchronize(streams[i]);
+    // for (int i = 0; i < numStream; i++)
+    //     cudaStreamSynchronize(streams[i]);
 
 
 
@@ -179,17 +179,17 @@ int main (int argc, char *argv[])
 
 
     // basicSgemm(matArow, matBcol, matBrow, A_d, B_d, C_d);
-    // for (int i = 0; i < numStream; i++)
-    // {
-    //     if (i != numStream-1)
-    //     {
-    //         basicSgemm(matArow/numStream, matArow/numStream, matArow/numStream, A_d[i], B_d[i], C_d[i], streams[i]);
-    //     }
-    //     else
-    //     {
-    //         basicSgemm(matArow/numStream + VecSize % numStream, matArow/numStream + VecSize % numStream,matArow/numStream + VecSize % numStream,A_d[i], B_d[i], C_d[i], streams[i]);
-    //     }
-    // }
+    for (int i = 0; i < numStream; i++)
+    {
+        if (i != numStream-1)
+        {
+            basicSgemm(matArow/numStream, matArow/numStream, matArow/numStream, A_d[i], B_d[i], C_d[i], streams[i]);
+        }
+        else
+        {
+            basicSgemm(matArow/numStream + VecSize % numStream, matArow/numStream + VecSize % numStream,matArow/numStream + VecSize % numStream,A_d[i], B_d[i], C_d[i], streams[i]);
+        }
+    }
 
 
 
@@ -210,7 +210,7 @@ int main (int argc, char *argv[])
 
     /*************************************************************************/
     //INSERT CODE HERE
-    // cudaMemcpy(C_h, C_d, sizeof(float) * C_sz, cudaMemcpyDeviceToHost);	
+    cudaMemcpy(C_h, C_d, sizeof(float) * C_sz, cudaMemcpyDeviceToHost);	
 
 
     // for (int i = 0; i < numStream; i++)

0bea63df0541ee68e309fcc1934bda4829d0eb3d
ashwinn <ashwinn@qburst.com>
2022-12-04 20:48:49 -0800

    Initial

--- main.cu
+++ main.cu
@@ -191,7 +191,7 @@ int main (int argc, char *argv[])
         else {
             Offset = (i * segmentLen) + VecSize % numStream;
             cudaMemcpyAsync(&A_d[Offset], &A_h[Offset], sizeof(float)*(segmentLen), cudaMemcpyHostToDevice, streams[i]);
-            cudaMemcpyAsync(&B_d[Offset], &B_h[Offset], sizeof(float)*(segmentLen + VecSize % numStream), cudaMemcpyHostToDevice, streams[i]);
+            cudaMemcpyAsync(&B_d[Offset], &B_h[Offset], sizeof(float)*(VecSize), cudaMemcpyHostToDevice, streams[i]);
             
             // basicSgemmStream(matArow/numStream, matArow/numStream, matArow/numStream, A_ds[Offset], B_ds[Offset], C_ds[Offset], streams[i]);
             basicSgemmStream(matArow,matArow,matArow, &A_d[Offset], B_d, &C_d[Offset], streams[i]);

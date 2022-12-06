4970cee8684a0fdaaafe940cd2bc7e59d0d6e5eb
ashwinn <ashwinn@qburst.com>
2022-12-05 16:55:57 -0800

    Initial

--- main.cu
+++ main.cu
@@ -93,6 +93,10 @@ int main (int argc, char *argv[])
     cudaMalloc((float **)&B_d, sizeof(float) * VecSize);
     cudaMalloc((float **)&C_d, sizeof(float) * VecSize);
 
+    // cudaMallocManaged(&A_d, sizeof(float) * VecSize)
+    // cudaMallocManaged(&B_d, sizeof(float) * VecSize)
+    // cudaMallocManaged(&C_d, sizeof(float) * VecSize)
+
 
     /*************************************************************************/
 	
@@ -106,7 +110,20 @@ int main (int argc, char *argv[])
     /*************************************************************************/
 
     //INSERT CODE HERE
-
+//   const int blockSize = 256, nStreams = 4;
+//   const int n = 4 * 1024 * blockSize * nStreams;
+//   const int streamSize = n / nStreams;
+//   const int streamBytes = streamSize * sizeof(float);
+//     for (int i = 0; i < nStreams; ++i) {
+//     int offset = i * streamSize;
+//     checkCuda( cudaMemcpyAsync(&d_a[offset], &a[offset], 
+//                                streamBytes, cudaMemcpyHostToDevice, 
+//                                stream[i]) );
+//     kernel<<<streamSize/blockSize, blockSize, 0, stream[i]>>>(d_a, offset);
+//     checkCuda( cudaMemcpyAsync(&a[offset], &d_a[offset], 
+//                                streamBytes, cudaMemcpyDeviceToHost,
+//                                stream[i]) );
+//   }
     for (int i = 0; i < numStream; i++)
     {
 
@@ -118,7 +135,7 @@ int main (int argc, char *argv[])
             
             basicSgemmStream(matArow,matArow,matArow, &A_d[Offset], B_d, &C_d[Offset], streams[i]);
 
-            cudaMemcpyAsync(&C_h[Offset], &C_d[Offset], sizeof(float)*segmentLen, cudaMemcpyDeviceToHost, streams[i]);
+            cudaMemcpyAsync(&C_h[Offset], &C_d[Offset], sizeof(float)*VecSize, cudaMemcpyDeviceToHost, streams[i]);
 
         }
         else {
